4842c4ba6483cff1e64cdb68318c2f355e5c5abb
patelprince0555 <patelprince0555@gmail.com>
2024-02-06 16:11:43 +0530

    06-02-2024

---
+++ 15-Javascript-Day-15/app2.js
@@ -0,0 +1,8 @@
+// conditional ststements
+//if-ststement
+console.log("before my if-ststement");
+let age=23;
+if(age>=18){
+    console.log("you can drive");
+}
+console.log("after my if-statement");
