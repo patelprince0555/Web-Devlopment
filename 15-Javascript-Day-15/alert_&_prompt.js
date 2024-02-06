daeae050c7067bb615bd872169026c2e3ccdc3e2
patelprince0555 <patelprince0555@gmail.com>
2024-02-06 22:41:56 +0530

    06-02-2024

---
+++ 15-Javascript-Day-15/alert_&_prompt.js
@@ -0,0 +1,9 @@
+alert("This is a simple alert")
+console.log("This is a simple log");
+console.error("This is simole error")
+console.warn("This is simole warning ")
+
+// prompt
+prompt("Enter your name:");
+let firstname=prompt("Enter your name");
+console.log(firstname)
---
+++ 15-Javascript-Day-15/practice_qs.js
@@ -0,0 +1,23 @@
+let day = 2;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+     case 2:
+        console.log("Tuesday");
+         break;
+     case 3:
+         console.log("wednesday");
+         break;
+    case 4:
+      console.log("thursday");
+         break;
+        case 5:
+     console.log("friday");
+         break;
+        case 6:
+      console.log("saturday");
+      break;
+   default:
+    console.log("sunday");
+}
