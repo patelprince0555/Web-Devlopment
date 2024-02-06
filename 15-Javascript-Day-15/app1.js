7cf3be81b4545892c0e19d26c38e35c7eea548f8
patelprince0555 <patelprince0555@gmail.com>
2024-02-06 14:53:32 +0530

    06-02-2024

---
+++ 15-Javascript-Day-15/app1.js
@@ -0,0 +1,7 @@
+let pencil = 10;
+let pen= 5;
+let output = "The total price is : "+(pencil+pen) + " rupees" ;
+console.log(output);
+console.log("The total price is :",(pen+pencil),"ruppes");
+// by use template literrals
+console.log(`The total price is : ${pen+pencil} rupees`);
