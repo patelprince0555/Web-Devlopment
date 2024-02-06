4006e364a9e5b9a83b855cb87b61cff736fb996e
patelprince0555 <patelprince0555@gmail.com>
2024-02-06 16:28:46 +0530

    06-02-2024

--- 15-Javascript-Day-15/app2.js
+++ 15-Javascript-Day-15/app2.js
@@ -6,3 +6,16 @@ if(age>=18){
     console.log("you can drive");
 }
 console.log("after my if-statement");
+
+// practice question
+//create a traffic light system that shows what to do based on color.
+let color="red";
+if(color=="red"){
+    console.log("Stop!");
+}
+if(color=="green"){
+    console.log("Ready!")
+}
+if(color=="green"){
+    console.log("Go!");
+}
