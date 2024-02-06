46531b01c76b2292244539e67aa0a7fc259e5d15
patelprince0555 <patelprince0555@gmail.com>
2024-02-06 21:10:58 +0530

    06-02-2024

---
+++ 15-Javascript-Day-15/logical_operators.js
@@ -0,0 +1,7 @@
+let str="apple";
+if(str[0]=='a'&&str.length>3){
+    console.log("good");
+}
+else{
+    console.log(bad);
+}
---
+++ 15-Javascript-Day-15/nested_if_else.js
@@ -0,0 +1,13 @@
+let marks =79;
+if(marks>=33){
+    console.log("pass");
+    if(marks>=80){
+        console.log("Grade: o");
+    }
+    else{
+        console.log("grade: A")
+    }
+}
+else{
+    console.log("Better luck for next time");
+}
