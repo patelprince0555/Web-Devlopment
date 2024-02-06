5cd53f2fa5be1d8eef98f464ab4a9ea94c39c556
patelprince0555 <patelprince0555@gmail.com>
2024-02-06 15:02:59 +0530

    06-02-2024

--- 15-Javascript-Day-15/app1.js
+++ 15-Javascript-Day-15/app1.js
@@ -5,3 +5,17 @@ console.log(output);
 console.log("The total price is :",(pen+pencil),"ruppes");
 // by use template literrals
 console.log(`The total price is : ${pen+pencil} rupees`);
+
+//Arithmetic operators
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// unary
+console.log(a++);
+console.log(++a);
