c0a508e5b2b1627f60bb7704677b2f463246bc96
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 17:16:33 +0530

    07-02-2024

---
+++ 15-Javascript-Day-15/practice_question.js
@@ -0,0 +1,36 @@
+// question 1
+let num =20;
+if(num%10==0){
+    console.log("good");
+}
+else{
+    console.log("bad");
+}
+
+// question 2
+let firstname=prompt("Enter your name");
+let age=prompt("Enter your age");
+let form=firstname+" is "+age+" years old.";
+let form1 = `${firstname} is ${age} years old`;
+alert(form1);
+
+
+// question 3
+let quarter = 3;
+switch(quarter){
+    case 1:
+        console.log("january, february, march");
+        break;
+        case 2:
+            console.log("april, may ,june")
+            break;
+            case 3:
+                console.log("july august september");
+                break;
+                default:
+                    console.log("october, november, december")
+}
+
+
+// question 4
+
