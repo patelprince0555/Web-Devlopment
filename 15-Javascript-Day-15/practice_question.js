b9c434d441faad6bd90080034fdb9c3aa3605a62
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 17:27:53 +0530

    07-02-2023

--- 15-Javascript-Day-15/practice_question.js
+++ 15-Javascript-Day-15/practice_question.js
@@ -8,11 +8,11 @@ else{
 }
 
 // question 2
-let firstname=prompt("Enter your name");
-let age=prompt("Enter your age");
-let form=firstname+" is "+age+" years old.";
-let form1 = `${firstname} is ${age} years old`;
-alert(form1);
+// let firstname=prompt("Enter your name");
+// let age=prompt("Enter your age");
+// let form=firstname+" is "+age+" years old.";
+// let form1 = `${firstname} is ${age} years old`;
+// alert(form1);
 
 
 // question 3
@@ -33,4 +33,11 @@ switch(quarter){
 
 
 // question 4
-
+let str = "appleee";
+console.log(str.length);
+if((str[0]=='A'||str[0]=='a')&&str.length>5){
+    console.log("Golden String");
+}
+else{
+    console.log("not");
+}
