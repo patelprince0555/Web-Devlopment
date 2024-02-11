304456e98b5e1b474672c68fdfd8fcc12b594ce5
patelprince0555 <patelprince0555@gmail.com>
2024-02-11 17:30:55 +0530

    11-02-2024

---
+++ 16-javascript-Day-16/Trim.js
@@ -0,0 +1,8 @@
+let str ="   hello   "
+console.log(str);
+console.log(str.trim());
+
+let password=prompt("Enter a password")
+console.log(password)
+let newpassword =password.trim();
+console.log(newpassword)
