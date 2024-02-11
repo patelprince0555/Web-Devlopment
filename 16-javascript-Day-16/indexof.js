ec6f9212f0eaff41ec89282e2acfeb3cd5375114
patelprince0555 <patelprince0555@gmail.com>
2024-02-11 18:44:09 +0530

    11-02-2024

--- 16-javascript-Day-16/indexof.js
+++ 16-javascript-Day-16/indexof.js
@@ -1,3 +1,10 @@
 let str = "i love coding"
 let str1 =str.indexOf("l");
 console.log(str1)
+
+
+// method chaining
+
+let str3 ="   hello prince  "
+let str4 = str3.trim().toUpperCase().indexOf("p");
+console.log(str4)
---
+++ 16-javascript-Day-16/slice.js
@@ -0,0 +1,16 @@
+let str = "ilovecoding";
+console.log(str.slice(5));
+console.log(str.slice(1,5));
+console.log(str.slice(-2));  // 10-2 = 8
+                             // console.log(str.slice(8))  
+
+
+// replce method
+let msg = "ilovecoding";
+console.log(msg.replace("love","prince"))     
+
+
+
+// repeat method
+let msg1 = "prince"+ " ";
+console.log(msg1.repeat(5));
