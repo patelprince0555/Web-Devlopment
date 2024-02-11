0f30c187d0881e1f7a996d5d5a003e4b1e8f256f
patelprince0555 <patelprince0555@gmail.com>
2024-02-11 20:28:07 +0530

    11-02-2024

---
+++ 16-javascript-Day-16/Array.js
@@ -0,0 +1,20 @@
+// let student1 = "prince";
+// let student2 ="ritika";
+//  let student3="rajat";
+
+let students=["prince","ritika","sohan"];
+console.log(students);
+console.log(students.length);
+
+
+let cars=["audi","bmw","xuv","maruti"];
+let cars1=cars.push("toyota");
+console.log(cars1);
+
+
+//  practice question
+// question1
+let start =['january','july','march','august'];
+let final=start.shift();
+console.log(final);
+console.log(start);
