27954726e745d3fdcd783c86a7a311e81a0e633e
patelprince0555 <patelprince0555@gmail.com>
2024-02-11 19:01:13 +0530

    11-02-2024

--- 16-javascript-Day-16/slice.js
+++ 16-javascript-Day-16/slice.js
@@ -14,3 +14,17 @@ console.log(msg.replace("love","prince"))
 // repeat method
 let msg1 = "prince"+ " ";
 console.log(msg1.repeat(5));
+
+
+// practice question
+
+// question1 
+let msg2 = "help!"
+console.log(msg2.trim().toUpperCase());
+
+
+// question2 
+let msg4= "apnacollege";
+let msg5 = msg4.slice(4);
+let msg6= msg5.replace("l","t").replace("l","t");
+console.log(msg6);
