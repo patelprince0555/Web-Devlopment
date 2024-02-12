142c334c0274f7cc344c25175e7d2020bb14aac1
patelprince0555 <patelprince0555@gmail.com>
2024-02-12 11:26:13 +0530

    12-02-2024

--- 16-javascript-Day-16/Array.js
+++ 16-javascript-Day-16/Array.js
@@ -18,3 +18,11 @@ let start =['january','july','march','august'];
 let final=start.shift();
 console.log(final);
 console.log(start);
+
+
+// slice
+let colors=["red","yellow","blue","orange","pink","white"];
+let colors2=["black","violet","purple"];
+let colors1=colors.concat(colors2);
+console.log(colors1);
+console.log(colors1.slice(2));
