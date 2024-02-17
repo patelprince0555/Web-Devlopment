89c305b504c52ea0f1f6432456ee5a7bef8b17e8
patelprince0555 <patelprince0555@gmail.com>
2024-02-17 20:27:49 +0530

    17-02-2024

---
+++ 17-javascript-Day-17/loops.js
@@ -0,0 +1,24 @@
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+// print number using loop
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+for(let i=5;i>=1;i--){
+   console.log(i);
+
+}
+
+//  print all odd numbers(1to15)
+let n=15;
+for(let i=1;i<=n;i++){
+    if(i%2!=0){
+        console.log(i)
+
+    }
+}
