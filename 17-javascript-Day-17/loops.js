c35823dca937b7881ff4b578d27c4512eb914eda
patelprince0555 <patelprince0555@gmail.com>
2024-02-17 20:38:49 +0530

    17-02-2024

--- 17-javascript-Day-17/loops.js
+++ 17-javascript-Day-17/loops.js
@@ -15,10 +15,27 @@ for(let i=5;i>=1;i--){
 }
 
 //  print all odd numbers(1to15)
-let n=15;
-for(let i=1;i<=n;i++){
-    if(i%2!=0){
-        console.log(i)
+// metod 1
+for(let j=1;j<=15;j=j+2){
+    console.log(j)
+}
+console.log("backwards")
+for(let k=21;k>=1;k=k-2){
+    console.log(k)
+}
 
-    }
+
+// let n=15;
+// for(let i=1;i<=n;i++){
+//     if(i%2!=0){
+//         console.log(i)
+
+//     }
+// }
+
+//
+console.log(" print all even numbers (2to10)")
+let n =10;
+for(let i=2;i<=10;i=i+2){
+    console.log(i)
 }
