e973f47bcff277b217b9a8f9936042d10605d8cf
patelprince0555 <patelprince0555@gmail.com>
2024-02-18 01:24:26 +0530

    18-02-2024

--- 14-Javascript-Day-14/index.js
+++ 14-Javascript-Day-14/index.js
@@ -1,1 +1,48 @@
-1
+//  variable
+// a variable is simply the name of the storage location
+
+//  Data types in js
+// 1 number
+// 2 boolean
+//  3 undefined
+// 4 null
+//  5 bigint
+//  6 symnbol
+
+
+// operation in js
+
+
+// NaN in js
+// -> the NaN globel property is a value representing Not-A-number
+
+
+
+
+// operator precendence
+
+
+// Types of operator
+// 1 Assignment operators
+// 2 Unary operator
+
+
+// identifier rules
+
+// camel case
+//  -> way of writing identifiers
+
+
+
+// String in js
+
+
+// String Indices  
+
+
+// concatenation
+// -> adding strings together
+
+
+
+// null and undefined in js
