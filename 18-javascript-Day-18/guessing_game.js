c8c386c1e13914219968d19de2db1854cc61b3ec
patelprince0555 <patelprince0555@gmail.com>
2024-02-20 02:55:48 +0530

    20-02-2024

---
+++ 18-javascript-Day-18/guessing_game.js
@@ -0,0 +1,19 @@
+const max =prompt("Enter the max number");
+const random=Math.floor(Math.random() * max)+1;
+let guess =prompt("guess the number");
+while(true){
+    if(guess=="quit"){
+        console.log("user quit");
+        break;
+    }
+    if(guess==random){
+        console.log("you are right! congrats!! random number is",random);
+        break;
+    }
+    else if(guess<random){
+        guess=prompt("Hint!: your guess was too small, please try again");
+    }
+    else{
+        guess=prompt("Hint!: your guess was too large. please try again")
+    }
+}
