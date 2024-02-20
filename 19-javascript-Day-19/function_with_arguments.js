c052a8c9ee1a3a5358821953e219c375b65eea9c
patelprince0555 <patelprince0555@gmail.com>
2024-02-21 03:55:40 +0530

    21-02-2024

--- 19-javascript-Day-19/function_with_arguments.js
+++ 19-javascript-Day-19/function_with_arguments.js
@@ -2,7 +2,23 @@ function printinfo(name , age){
     console.log(name +" age is"+age);
 
 }
-
-
 printinfo("prince" ,21);
 printinfo("ritika" ,21);
+
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(1,5);
+sum(8,90);
+
+// Create a function that prints the multiplication table of a number
+function printtable(n){
+    for(let i=n;i<=n*10;i=i+n){
+        console.log(i);
+
+    }
+}
+
+printtable(10);
