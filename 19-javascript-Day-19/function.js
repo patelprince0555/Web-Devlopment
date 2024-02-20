1363647d46bfe3512ff5d2d37636a2194bf9121a
patelprince0555 <patelprince0555@gmail.com>
2024-02-21 03:28:26 +0530

    21-02-2024

--- 19-javascript-Day-19/function.js
+++ 19-javascript-Day-19/function.js
@@ -6,3 +6,14 @@ hello();
 hello();
 hello();
 hello();
+
+
+
+
+//  Create a function that prints a poem
+function printpeom(){
+    console.log("Twinkle Twinkle, little star");
+    console.log("how i wonder what you are");
+}
+
+printpeom();
