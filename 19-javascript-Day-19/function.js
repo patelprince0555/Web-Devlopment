d56cfdd971e4cf29a1bbfd0cbe533ab2e1b93636
patelprince0555 <patelprince0555@gmail.com>
2024-02-21 03:35:04 +0530

    21-02-2024

--- 19-javascript-Day-19/function.js
+++ 19-javascript-Day-19/function.js
@@ -17,3 +17,14 @@ function printpeom(){
 }
 
 printpeom();
+
+
+//  Create a funcation to roll a dice & display the value of the dice (1 to 6)
+
+function printdice(){
+   let dice= Math.floor(Math.random()*6)+1;
+   console.log(dice)
+}
+
+printdice();
+printdice();
