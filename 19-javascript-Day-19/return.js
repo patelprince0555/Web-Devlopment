c7f1b95fbd55feac4a5bead9679522d5456f96ce
patelprince0555 <patelprince0555@gmail.com>
2024-02-22 01:58:25 +0530

    22-02-2024

--- 19-javascript-Day-19/return.js
+++ 19-javascript-Day-19/return.js
@@ -1,6 +1,19 @@
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(2,5));
+// console.log(sum(sum(2,4),4));
+
+
+
+//  create a function that returs the sum of the numbers from 1 to n
+function getsum(n){
+    let sum=0;
+    for(let i=0;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
 }
 
-console.log(sum(2,5));
-console.log(sum(sum(2,4),4));
+console.log(getsum(5));
