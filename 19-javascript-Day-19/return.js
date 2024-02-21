6413d06b43c8da8405e0ae0cbcc621c6be3ee176
patelprince0555 <patelprince0555@gmail.com>
2024-02-22 02:12:30 +0530

    22-02-2024

--- 19-javascript-Day-19/return.js
+++ 19-javascript-Day-19/return.js
@@ -17,3 +17,18 @@ function getsum(n){
 }
 
 console.log(getsum(5));
+
+
+//  create a function that returns the concatenation of all strings in an array.
+let str =["hi","hello","bye","!"];
+
+function concat(str){
+    let result="";
+
+    for(let i=0;i<str.length;i++){
+        result+=str[i];
+    }
+    return result;
+}
+
+console.log(concat(str))
