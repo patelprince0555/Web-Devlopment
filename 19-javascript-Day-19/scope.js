15050d4d23d40feee4194aa95da2990c85aaf865
patelprince0555 <patelprince0555@gmail.com>
2024-02-22 20:49:18 +0530

    22-02-2024

--- 19-javascript-Day-19/scope.js
+++ 19-javascript-Day-19/scope.js
@@ -3,7 +3,32 @@ let sum =54; // global scope
 function calsum(a,b){
     let sum =a+b;
     console.log(sum);
+    // function block
 }
 
 calsum(1,3);
 console.log(sum);
+
+
+
+// block scope
+{
+    let a=90;
+    // it is a block scope
+
+}
+// console.log(a);
+
+
+
+// lexical scope
+function outerfunc(){
+    console.log(x);
+
+    function innnerfunc(){
+        let x=9;
+        let y=8;
+        //  it is a lexical scope
+    }
+    innnerfunc();
+}
