9b25c320a38a4cfadba5568cdbfb12d9f2c66c18
patelprince0555 <patelprince0555@gmail.com>
2024-02-23 01:23:01 +0530

    23-02-2024

--- 19-javascript-Day-19/function_Expressions.js
+++ 19-javascript-Day-19/function_Expressions.js
@@ -1,7 +1,41 @@
 let name ="prince";
 
 let sum= function(a,b){
-    return sum;
+    return a+b;
 }
 
-console.log(sum);
+console.log(sum(5,7));
+
+
+//  higher value funcations
+
+function multiplegreet(func,count){
+    for(let i=1;i<=count;i++){
+        func();
+    }
+}
+let greet=function(){
+    console.log("hello prince");
+}
+
+multiplegreet(greet,20);
+
+
+// higher order functions(returns)
+function oddorevenfactory(request){
+    if(request=="odd"){
+        let odd=function(n){
+            console.log(!(n%2==0));
+        }
+        return odd;
+    }else if(request=="even"){
+        let even =function(n){
+            console.log(n%2==0);
+        }
+        return even;
+    } else{
+        console.log("wrong request");
+    }
+}
+
+let request="odd";
