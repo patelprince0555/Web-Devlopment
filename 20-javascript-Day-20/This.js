8210b3bf2b419438d999c6cbc763a8e401d433b3
patelprince0555 <patelprince0555@gmail.com>
2024-02-28 14:24:02 +0530

    28-02-2024

--- 20-javascript-Day-20/This.js
+++ 20-javascript-Day-20/This.js
@@ -2,12 +2,12 @@ const student ={
     name:"prince",
     age:23,
     eng:95,
-    math:97,
+    math:93,
     phy:97,
     getAvg(){
-        console.log(this);
+        // console.log(this);
         let avg =(this.eng+this.math+this.phy)/3;
-        console.log('${this.name} got avg marks=${avg}');
+        console.log(`${this.name} got avg marks=${avg}`);
     }
 }
-console.log(student);
+console.log(student.getAvg());
