7c4529d27f7545e448681376a1f87c7557597819
patelprince0555 <patelprince0555@gmail.com>
2024-02-28 14:53:00 +0530

    28-02-2024

---
+++ 20-javascript-Day-20/Arrow_functions.js
@@ -0,0 +1,11 @@
+const sum =(a,b)=>{
+    console.log(a+b);
+}
+
+const cube=(n)=>{
+    return n*n*n;
+};
+
+const pow =(a,b)=>{
+    return a**b;
+};
---
+++ 20-javascript-Day-20/try_and_catch.js
@@ -0,0 +1,13 @@
+console.log("hello");
+console.log("hello");
+try{
+    console.log(a);
+}catch{
+    console.log("caught an error.. a is not defined")
+}
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+
+
