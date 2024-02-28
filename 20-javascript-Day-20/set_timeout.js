d164702e925ff1f47bb8bac86f789fa32250ff16
patelprince0555 <patelprince0555@gmail.com>
2024-02-28 17:11:26 +0530

    28-02-2024

--- 20-javascript-Day-20/set_timeout.js
+++ 20-javascript-Day-20/set_timeout.js
@@ -6,3 +6,39 @@ console.log("Welcome to");
 console.log("Welcome to");
 console.log("Welcome to");
 console.log("Welcome to");
+
+
+// set interval
+ let id =setInterval(()=>{
+    console.log("Apna college");
+} , 4000);
+
+console.log(id);
+
+
+
+//  this with arrow functions
+const student ={
+    name:"prince",
+    marks:95,
+    prop:this,
+    getName:function(){
+        console.log(this);// parents scope-> window
+        return this.marks;
+    },
+    getMarks:()=>{
+        console.log(this);
+        return this.marks;
+    },
+    getinfo1:function(){
+        setTimeout(()=>{
+            console.log("this");
+        } ,2000);
+    },
+
+    getinfo2:function(){
+        setTimeout( function(){
+            console.log(this);
+        } ,2000);
+    }
+}
