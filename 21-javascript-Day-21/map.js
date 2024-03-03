c4ac98a2f34bb48ae9d77630de6ad1062a3a9f3e
patelprince0555 <patelprince0555@gmail.com>
2024-03-04 02:50:53 +0530

    04-03-2024

--- 21-javascript-Day-21/map.js
+++ 21-javascript-Day-21/map.js
@@ -22,3 +22,11 @@ let gpa =student.map((el)=>{
     return el.marks/10;
 });
 console.log(gpa);
+
+
+// filter
+let nums=[2,4,1,5,6,2,7,8,9];
+let even =nums.filter((el)=>{
+    return el%2==0;
+});
+console.log(even);
