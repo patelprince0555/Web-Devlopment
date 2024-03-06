57df089c1ea9667315d2598de12b2b9398e74b21
patelprince0555 <patelprince0555@gmail.com>
2024-03-06 11:32:05 +0530

    06-03-2024

--- 21-javascript-Day-21/Spread.js
+++ 21-javascript-Day-21/Spread.js
@@ -1,1 +1,12 @@
 console.log(..."apnacollege"); 
+let arr=[1,2,3,4,5];
+let newarr=[...arr];
+
+newarr.push(4);
+console.log(newarr);
+console.log(arr);
+
+let even=[2,4,6,8,10];
+let odd=[1,3,5,7,9];
+let arr1=[...odd,...odd];
+console.log(arr1);
