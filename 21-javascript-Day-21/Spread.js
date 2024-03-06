78c301401eabadafbd77568bd2f13bb557464d3b
patelprince0555 <patelprince0555@gmail.com>
2024-03-06 11:42:46 +0530

    06-03-2024

--- 21-javascript-Day-21/Spread.js
+++ 21-javascript-Day-21/Spread.js
@@ -10,3 +10,12 @@ let even=[2,4,6,8,10];
 let odd=[1,3,5,7,9];
 let arr1=[...odd,...odd];
 console.log(arr1);
+
+
+// spread with object literals
+let data ={
+    email:"patelprince0555@gmail.com",
+    password:1234
+}
+let datacopy={...data , country:"india",id:"patelprince0555"};
+console.log(datacopy);
