ad03873e2f5ebd24eb9b0d19ecf1441ffd435a6a
patelprince0555 <patelprince0555@gmail.com>
2024-04-02 19:57:43 +0530

    02-04-2024

--- 25-javascript-Day-25/App.js
+++ 25-javascript-Day-25/App.js
@@ -1,4 +1,12 @@
 function hello(){
+    console.log("inside hello fnx");
     console.log("hello")
 }
-hello();
+
+function demo(){
+    console.log("calling hello fnx");
+    hello();
+}
+console.log("calling demo fnx");
+demo();
+console.log("done,bye!");
