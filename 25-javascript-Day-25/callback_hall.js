f57a439cecf87b098f14ba16090f88f3776b3e1b
patelprince0555 <patelprince0555@gmail.com>
2024-05-06 22:50:54 +0530

    06-05-2024

--- 25-javascript-Day-25/callback_hall.js
+++ 25-javascript-Day-25/callback_hall.js
@@ -8,12 +8,12 @@ function changecolor(color,delay){
     });
 }
 
-// changecolor("red",1000,()=>{
-//     changecolor("orange",1000 ,()=>{
-//         changecolor("green",1000,()=>{
-//             changecolor("blue",1000,()=>{
-//                 changecolor("blue",1000);
-//             })
-//         })
-//     })
-// })
+changecolor("red",1000,()=>{
+    changecolor("orange",1000 ,()=>{
+        changecolor("green",1000,()=>{
+            changecolor("blue",1000,()=>{
+                changecolor("blue",1000);
+            })
+        })
+    })
+})
