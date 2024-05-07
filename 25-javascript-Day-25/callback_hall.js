392c80352f019e33bb3165f25b19be1e3dc92a40
patelprince0555 <patelprince0555@gmail.com>
2024-05-07 11:50:00 +0530

    07-05-2024

--- 25-javascript-Day-25/callback_hall.js
+++ 25-javascript-Day-25/callback_hall.js
@@ -8,12 +8,31 @@ function changecolor(color,delay){
     });
 }
 
-changecolor("red",1000,()=>{
-    changecolor("orange",1000 ,()=>{
-        changecolor("green",1000,()=>{
-            changecolor("blue",1000,()=>{
-                changecolor("blue",1000);
-            })
-        })
-    })
+
+changecolor("red",1000)
+.then(()=>{
+    console.log("red color was completed");
+    return changecolor("orange",1000)
 })
+.then(()=>{
+    console.log("orange color was completed");
+    return changecolor("green",1000)
+})
+.then(()=>{
+    console.log("green color was completed");
+    return changecolor("blue",1000)
+})
+.then(()=>{
+    console.log("blue color was completed");
+  
+});
+
+// changecolor("red",1000,()=>{
+//     changecolor("orange",1000 ,()=>{
+//         changecolor("green",1000,()=>{
+//             changecolor("blue",1000,()=>{
+//                 changecolor("blue",1000);
+//             })
+//         })
+//     })
+// })
