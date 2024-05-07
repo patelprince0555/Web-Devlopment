803638dec571dffb74c266e2bbeff2054f5f74aa
patelprince0555 <patelprince0555@gmail.com>
2024-05-07 10:41:49 +0530

    07-05-2024

--- 25-javascript-Day-25/promises.js
+++ 25-javascript-Day-25/promises.js
@@ -1,38 +1,38 @@
-// function savetodb(data,success,failure){
-//     let internetSpeed=Math.floor(Math.random()*10)+1;
-//     if(internetSpeed>4){
-//         success();
-//     }else{
-//         failure();
+function savetodb(data,success,failure){
+    let internetSpeed=Math.floor(Math.random()*10)+1;
+    if(internetSpeed>4){
+        success();
+    }else{
+        failure();
 
-//     }
-// }
+    }
+}
 
-// savetodb("apna college",
-// ()=>{
-//     console.log(" success : your data was saved");
-//     savetodb(
-//         "hello world",
-//         () => {
-//         console.log("success 2 : data was saved");
-//         savetodb(
-//             "prince",
-//             ()=>{
-//             console.log("success 3: data was saved");
-//         },
-//         ()=>{
-//             console.log("failure 3: weak connection data was not saved");
-//         }
-//     );
-//     },
-//     ()=>{
-//         console.log("failure 2 :weak connection data was not saved");
-//     } 
-//    );
-//     }, 
-//   ()=>{
-//     console.log(" Failure : weak connection. data not saved");
-// });
+savetodb("apna college",
+()=>{
+    console.log(" success : your data was saved");
+    savetodb(
+        "hello world",
+        () => {
+        console.log("success 2 : data was saved");
+        savetodb(
+            "prince",
+            ()=>{
+            console.log("success 3: data was saved");
+        },
+        ()=>{
+            console.log("failure 3: weak connection data was not saved");
+        }
+    );
+    },
+    ()=>{
+        console.log("failure 2 :weak connection data was not saved");
+    } 
+   );
+    }, 
+  ()=>{
+    console.log(" Failure : weak connection. data not saved");
+});
 
 
 function savetodb(data){
