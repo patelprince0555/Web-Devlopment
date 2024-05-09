bfee2bdd4612224d185bb35ce8fcc91e27b6f6dc
patelprince0555 <patelprince0555@gmail.com>
2024-05-09 17:45:37 +0530

    09-05-2024

---
+++ 27-javascript-Day-27/App.js
@@ -0,0 +1,23 @@
+
+let button =document.querySelector("button");
+button.addEventListener("click",async()=>{
+    let fact= await getfacts();
+    console.log(fact);
+    let p =document.querySelector("#result");
+    p.innerText=fact;
+});
+
+
+
+let url="https://catfact.ninja/fact";
+async function getfacts(){
+    try{
+        let res=await axios.get(url);
+        return res.data.fact;
+    }catch(e){
+        console.log("error-",e);
+        return "No fact found";
+    }
+}
+// let u =getfacts();
+// console.log(u);
