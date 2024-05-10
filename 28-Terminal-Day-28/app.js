ffb968c2cbe96c0a8345e2d284a2b104f9df6698
patelprince0555 <patelprince0555@gmail.com>
2024-05-10 19:00:55 +0530

    10-05-2024

--- 28-Terminal-Day-28/app.js
+++ 28-Terminal-Day-28/app.js
@@ -1,3 +1,12 @@
 // in this part js we learn only terminal 
 // in terminal how to make a folder and new thing 
 // so in part learn about terminal
+
+// different terms
+// basics command
+// navigation
+// paths
+// making directories
+// flags
+// touch command
+// deleting files & folders
