82c1adb0ef736168feebbf65c0ab92466494355a
patelprince0555 <patelprince0555@gmail.com>
2024-05-12 14:23:53 +0530

    12-05-2024

---
+++ 29-Git-&-Github-Day-29/app.js
@@ -0,0 +1,19 @@
+// git & github
+// to check git -- version
+// configuring git
+// git clone <some link>
+// git status
+// git add <file name>
+// git commit -m "some message"
+// git push origin main
+
+
+//Init command
+//git branches
+// branch commands
+//merging code
+// pull request
+// merge conflicts
+// fixing mistakes
+// what is forking
+       // fork is rough copy
