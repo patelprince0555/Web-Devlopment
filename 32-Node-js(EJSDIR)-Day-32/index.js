44eb2a39ff832ad86ec4d48de9c4ca8dd2f08f84
patelprince0555 <patelprince0555@gmail.com>
2024-05-25 19:57:53 +0530

    25-05-2024

--- 32-Node-js(EJSDIR)-Day-32/index.js
+++ 32-Node-js(EJSDIR)-Day-32/index.js
@@ -3,6 +3,8 @@ const app =express();
 const port =3000;
 const path=require("path");
 
+
+app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.get("/",(req,res)=>{
