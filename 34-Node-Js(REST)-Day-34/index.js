8d94737e1f0a362cd16c7552ea9bd7c3080ad3d9
patelprince0555 <patelprince0555@gmail.com>
2024-05-29 00:23:07 +0530

    29-05-2024

--- 34-Node-Js(REST)-Day-34/index.js
+++ 34-Node-Js(REST)-Day-34/index.js
@@ -8,11 +8,26 @@ app.use(express.urlencoded({extended: true}));
 app.set ("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static (path.join(__dirname ,"public")));
+app.use(express.static (path.join(__dirname ,"public")));
+
+let posts=[
+    {
+        username:"apnacollege",
+        content:"i love coding"
+    },
+    {
+        username:"prince patel",
+        content:"hard work is important to achieve sucess",
+    },
+    {
+        username:"rahul kumar",
+        content:"i got my first internship!"
+    }
+]
 
 
-app.set("/",(req ,res)=>{
-    console.log(" serving working well!");
+app.get("/posts",(req ,res)=>{
+    res.render("index.ejs",{posts});
 })
 
 app.listen(port,()=>{
