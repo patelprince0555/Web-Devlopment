f439c5795138b4419eb3f1be0c8033b0f1786bfb
patelprince0555 <patelprince0555@gmail.com>
2024-05-30 22:10:20 +0530

    30-05-2024

---
+++ 35-SQL-Day-35/index.js
@@ -0,0 +1,19 @@
+// SQL (structured query language)
+// Topic 
+// what is a database
+// why databases
+// SQL VS NOSQL
+// Table in sql
+// crate database
+// drop database
+// table queries
+   //create
+   // insert
+   //update
+   //alter
+   //truncate
+   //delete
+// datatypes
+//constraints
+  // primary name
+  // foreign key    
