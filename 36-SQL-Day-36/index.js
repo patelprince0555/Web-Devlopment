33f6592c28eb758718e1c09b705c1123aa4338c3
patelprince0555 <patelprince0555@gmail.com>
2024-06-01 00:25:31 +0530

    01-06-2024

---
+++ 36-SQL-Day-36/index.js
@@ -0,0 +1,14 @@
+// where clause
+// operators
+// aggregate functions
+// group by clause
+// having clause
+//general order
+//table queries 
+        //update
+        // delte
+        // add
+        // drop column
+        //rename table
+        // chabge column
+        // modify column
