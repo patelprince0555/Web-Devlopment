e17d6cfb31dcf323b5166562c9835fce1077aff1
patelprince0555 <patelprince0555@gmail.com>
2024-07-04 19:17:13 +0530

    04-07-2024

--- 38-MongoDB-Day-38/index.js
+++ 38-MongoDB-Day-38/index.js
@@ -1,1 +1,15 @@
 // In this lecture we crud opertion in mongoDb
+//the shell
+   // mongosh
+   //use database name
+   //quit
+   //help
+   //show dbs
+   //db
+
+// json vs bson
+
+// insert in db
+
+// find in db
+ //Query operators
