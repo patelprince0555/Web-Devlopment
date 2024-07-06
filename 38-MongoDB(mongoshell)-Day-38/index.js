67f2a2fc5e843f1ff971cc1cbeae8aca9aabde92
patelprince0555 <patelprince0555@gmail.com>
2024-07-06 15:44:03 +0530

    06-07-2024

---
+++ 38-MongoDB(mongoshell)-Day-38/index.js
@@ -0,0 +1,26 @@
+// In this lecture we crud opertion in mongoDb
+//the shell
+   // mongosh
+   //use database name
+   //quit
+   //help
+   //show dbs
+   //db
+
+// json vs bson
+
+// insert in db
+
+// find in db
+ //Query operators
+// update in database
+   //updateone
+   //updatemany
+   // replaceone
+// nesting
+// delete in database
+   // deletemany
+   // deleteone
+
+// delete database
+  //dp.dropdatabase()
