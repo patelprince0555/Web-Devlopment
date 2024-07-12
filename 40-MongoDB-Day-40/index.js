66964fbd6eac7d2f3b0759eb23dcf131e7212f9f
patelprince0555 <patelprince0555@gmail.com>
2024-07-12 17:35:21 +0530

    12-07-2024

--- 40-MongoDB-Day-40/index.js
+++ 40-MongoDB-Day-40/index.js
@@ -2,6 +2,8 @@ const express =require("express");
 const app=express();
 const mongoose=require("mongoose");
 const path = require("path");
+
+
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 
