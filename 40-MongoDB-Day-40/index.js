2f0d4dbca7137b4127d99c136bf761e35f2302c4
patelprince0555 <patelprince0555@gmail.com>
2024-07-16 16:31:48 +0530

    16-07-2024

--- 40-MongoDB-Day-40/index.js
+++ 40-MongoDB-Day-40/index.js
@@ -8,6 +8,7 @@ const Chat=require("./models/chat.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main().then(()=>{
     console.log("connection successful");
@@ -30,15 +31,36 @@ async function main() {
 //     console.log(res);
 // })
 
+//index Route
 app.get("/chats", async(req,res)=>{
     let chats= await Chat.find();
     console.log(chats);
     res.render("index.ejs",{chats});
 });
 
+// new route
 app.get("/chats/new",(req ,res)=>{
 res.render("new.ejs");
 })
+
+
+// Create Route
+app.post("/chats",(req,res)=>{
+    let{from,to,message}=req.body;
+    let newChat= new Chat({
+        from:from,
+        to:to,
+        message:message,
+        Created_at:new Date()
+    });
+    newChat.save().then((res)=>{
+        console.log("chat was saved");
+    }).catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chats");
+})
+
 
 app.get("/",(req,res)=>{
     res.send("root is working");
