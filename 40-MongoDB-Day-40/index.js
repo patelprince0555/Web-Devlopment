666939fc854c2a45c4f3d65d1ab55a63feac287f
patelprince0555 <patelprince0555@gmail.com>
2024-07-16 18:37:37 +0530

    16-07-2024

--- 40-MongoDB-Day-40/index.js
+++ 40-MongoDB-Day-40/index.js
@@ -62,6 +62,14 @@ app.post("/chats",(req,res)=>{
 })
 
 
+// Edit Route
+app.get("/chats/:id/edit" ,async (req,res)=>{
+    let {id}=req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+})
+
+
 app.get("/",(req,res)=>{
     res.send("root is working");
 })
