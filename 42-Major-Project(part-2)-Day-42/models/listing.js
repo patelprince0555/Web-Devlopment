542d994356067bd4d71df24079905b0bd42f7436
patelprince0555 <patelprince0555@gmail.com>
2024-07-22 23:03:15 +0530

    22-07-2024

--- 42-Major-Project(part-2)-Day-42/models/listing.js
+++ 42-Major-Project(part-2)-Day-42/models/listing.js
@@ -11,8 +11,8 @@ const listingSchema=new Schema({
     },
     image:{
         type:String,
-        default:"https://unsplash.com/photos/a-small-cabin-in-the-middle-of-a-forest-hzEJGVijmVU",
-        set:(v)=>v===""?"https://unsplash.com/photos/a-small-cabin-in-the-middle-of-a-forest-hzEJGVijmVU":v,
+        default:"https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        set:(v)=>v===""?"https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60":v,
     },
     price:{
         type:Number
