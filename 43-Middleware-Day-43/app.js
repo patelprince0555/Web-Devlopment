55180e5dcd91b7befce83f3803bb60a0867eb924
patelprince0555 <patelprince0555@gmail.com>
2024-07-27 20:34:39 +0530

    27-07-2024

--- 43-Middleware-Day-43/app.js
+++ 43-Middleware-Day-43/app.js
@@ -1,2 +1,21 @@
 const express=require("express");
 const app=express();
+
+
+app.use((req,res)=>{
+    let{query}=req.query;
+    console.log(query);
+    console.log("Hi, i am middleware");
+    res.send("middleware finished");
+})
+app.get("/",()=>{
+    console.log("Hi, I am root");
+})
+
+app.get("/random",()=>{
+    console.log("This is random page");
+})
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+})
