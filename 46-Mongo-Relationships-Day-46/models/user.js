e6008232baf72805d42d7168cd5348bbb902b944
patelprince0555 <patelprince0555@gmail.com>
2024-08-17 21:51:44 +0530

    17-08-2024

--- 46-Mongo-Relationships-Day-46/models/user.js
+++ 46-Mongo-Relationships-Day-46/models/user.js
@@ -10,12 +10,32 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-const userSchema=new Schema({
+const userSchema=new  mongoose.Schema({
     username:String,
     addresses:[
         {
+            _id:false,
             location:String,
             city:String,
         },
     ],
 });
+
+const User = mongoose.model("User",userSchema);
+
+const addUsers = async()=>{
+    let user1=new User({
+        username:"sherlockholmes",
+        addresses:[
+            {
+                location:"221B Baker Street",
+                city:"London",
+            },
+        ],
+    });
+    user1.addresses.push({location:"p32 wallStreet",city:"London"});
+    let result = await user1.save();
+    console.log(result);
+};
+
+addUsers();
