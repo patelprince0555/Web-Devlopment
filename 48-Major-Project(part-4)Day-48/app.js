4902ebd328355007975a0c6ee586c3aa0fb1a395
patelprince0555 <patelprince0555@gmail.com>
2024-08-24 14:21:30 +0530

    24-08-2024

---
+++ 48-Major-Project(part-4)Day-48/app.js
@@ -0,0 +1,65 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsmate= require("ejs-mate");
+const ExpressError=require("./utils/ExpressError.js");
+
+const listings= require("./routes/listing.js");
+const reviews=require("./routes/review.js")
+
+
+main().then((res)=>{
+    console.log("connected to DB");
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderLust');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsmate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+app.get("/",(req,res)=>{
+    res.send("Hi, i am root");
+})
+
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
+  
+// app.get("/listing", async (req,res)=>{
+// let sampleListing=new Listing({
+//     title:"my new villa",
+//     description:"by the beach",
+//     price:1200,
+//     location:"delhi rohini",
+//     country:"india",
+// });
+//  await sampleListing.save();
+// console.log("data was saved");
+// res.send("succesful testing");
+// })
+
+app.all("*",(req,res,next)=>{
+  next(new ExpressError(404,"Page not found!"));
+});
+
+
+app.use((err,req,res,next)=>{
+  let {statusCode=500 , message="something went wrong"}=err;
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs",{message});
+});
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+});
