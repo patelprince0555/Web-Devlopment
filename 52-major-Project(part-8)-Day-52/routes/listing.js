992cee7c25e2f0e576d930980d83c70ccc52f4be
patelprince0555 <patelprince0555@gmail.com>
2024-09-13 00:48:53 +0530

    13-09-2024

--- 52-major-Project(part-8)-Day-52/routes/listing.js
+++ 52-major-Project(part-8)-Day-52/routes/listing.js
@@ -1,3 +1,4 @@
+
 const express=require("express");
 const router=express.Router();
 const wrapAsync=require("../utils/wrapAsync.js");
@@ -5,11 +6,15 @@ const Listing=require("../models/listing.js");
 const {isLoggedIn, isOwner,validateListing}=require("../middleware.js");
 
 const listingController=require("../controllers/listings.js");
+const multer=require('multer')
+const {storage}=require("../cloudConfig.js");
+const upload=multer({storage});
 
 
 router.route("/")
 .get(wrapAsync(listingController.index))
   .post(isLoggedIn,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync (listingController.createListing)
   );
