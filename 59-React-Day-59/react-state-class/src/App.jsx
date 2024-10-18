31d61b6db7e63fbb4b8f563d8a3b8d6147d9ce1e
patelprince0555 <patelprince0555@gmail.com>
2024-10-18 21:00:54 +0530

    18-10-2024

--- 59-React-Day-59/react-state-class/src/App.jsx
+++ 59-React-Day-59/react-state-class/src/App.jsx
@@ -1,12 +1,11 @@
 
 import './App.css'
-import Counter from './Counter'
-import LikeButton from './LikeButton'
+import LudoBoard from './LudoBoard'
 
 function App() {
   return (
     <>
-     
+     <LudoBoard/>
     </>
   )
 }
