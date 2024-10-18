3ededadbc60cc72c6bfa029ed089d9cc6e1312af
patelprince0555 <patelprince0555@gmail.com>
2024-10-19 00:27:01 +0530

    19-10-2024

---
+++ 59-React-Day-59/react-state-class/src/TodoList.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export default function TodoList(){
+    let [todos , setTodos]=useState(["sample Task"]);
+    let [newTodo, setNewTodo]=useState("");
+
+    let addNewTask = ()=>{
+        setTodos([...todos,newTodo]);
+        setNewTodo("");
+    };
+
+    let updateTodoValue=(event)=>{
+        setNewTodo(event.target.value);
+    };
+    return(
+        <div>
+            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
+            <br></br>
+            <button onClick={addNewTask}>Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+
+
+            <hr></hr>
+            <h4>Tasks Todo</h4>
+            <ul>
+                {todos.map((todo)=>(
+                    <li>{todo}</li>
+                ))}
+            </ul>
+
+        </div>
+    );
+};
