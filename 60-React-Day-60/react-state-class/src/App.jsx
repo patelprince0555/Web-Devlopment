24be5abc4f49d8ef15042097899d6eebdd8502eb
patelprince0555 <patelprince0555@gmail.com>
2024-10-22 01:31:31 +0530

    22-10-2024

--- 60-React-Day-60/react-state-class/src/App.jsx
+++ 60-React-Day-60/react-state-class/src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
+import Lottery from './Lottery'
 
 function App() {
   return (
     <>
-     <TodoList/>
+     <Lottery/>
     </>
   )
 }
