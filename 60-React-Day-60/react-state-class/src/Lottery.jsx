31b741a1d82dc934afb90b16b078512d12ae9f65
patelprince0555 <patelprince0555@gmail.com>
2024-10-22 01:40:43 +0530

    22-10-2024

--- 60-React-Day-60/react-state-class/src/Lottery.jsx
+++ 60-React-Day-60/react-state-class/src/Lottery.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import { genTicket } from "./helper"
+import "./Lottery.css";
+import { genTicket ,sum } from "./helper"
 
 export default function Lottery(){
     let [ticket,setTicket]=useState(genTicket(3));
+    let isWinning = sum(ticket)===15;
     return(
         <div>
             <h1>Lottery Game!</h1>
@@ -11,6 +13,7 @@ export default function Lottery(){
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <h3>{isWinning && "congratulations, you won!"}</h3>
         </div>
     )
 }
