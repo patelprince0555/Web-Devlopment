91a96146b57357dc7ea3156618b3e045b0537e21
patelprince0555 <patelprince0555@gmail.com>
2024-10-23 00:57:18 +0530

    23-10-2024

--- 60-React-Day-60/react-state-class/src/App.jsx
+++ 60-React-Day-60/react-state-class/src/App.jsx
@@ -3,13 +3,11 @@ import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
 import Lottery from './Lottery'
-import TicketNum from './TicketNum'
-import Ticket from './Ticket'
 
 function App() {
   return (
     <>
-    <Ticket ticket={[0,1,2]}/>
+    <Lottery n={3} winningSum={18}/>
     </>
   )
 }
--- 60-React-Day-60/react-state-class/src/Lottery.jsx
+++ 60-React-Day-60/react-state-class/src/Lottery.jsx
@@ -1,23 +1,19 @@
 import { useState } from "react"
-import "./Lottery.css";
+// import "./Lottery.css";
 import { genTicket ,sum } from "./helper"
+import Ticket from "./Ticket";
 
-export default function Lottery(){
-    let [ticket,setTicket]=useState(genTicket(3));
-    let isWinning = sum(ticket)===15;
+export default function Lottery({n=3,winningSum=15}){
+    let [ticket,setTicket]=useState(genTicket(n));
+    let isWinning = sum(ticket)===winningSum;
 
     let buyTicket =()=>{
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     }
     return(
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br></br>
+            <Ticket ticket={ticket}/>
             <div className="button">
             <button onClick={buyTicket} style={{backgroundColor:"black"}}>Buy new ticket</button>
             </div>
