1e9b2873025c99c24239eab1296983cb2b04497a
patelprince0555 <patelprince0555@gmail.com>
2024-10-24 12:42:08 +0530

    24-10-2024

--- 61-React-Day-61/react-state-class2/src/Form.jsx
+++ 61-React-Day-61/react-state-class2/src/Form.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 export default function Form(){
+    let[fullName ,setFullName]=useState("prince");
+
+    let handleNameChange=(event)=>{
+        setFullName(event.target.value);
+    }
     return(
         <div>
-            <input placeholder="Enter your name"></input>
+            <input placeholder="Enter your name" 
+            type="text"
+             value={fullName}
+              onChange={handleNameChange}/>
+            <button>Submit</button>
         </div>
     )
 }
