3c6f115635a534476624cc7af42c7f389fd184d2
patelprince0555 <patelprince0555@gmail.com>
2024-10-25 00:01:21 +0530

    25-10-2024

--- 61-React-Day-61/react-state-class2/src/CommentsForm.jsx
+++ 61-React-Day-61/react-state-class2/src/CommentsForm.jsx
@@ -1,14 +1,52 @@
+import { useState } from "react"
 export default function CommentForm(){
+    let [formData, setformData]=useState({
+        username:"",
+        remarks:"",
+        rating:"",
+    });
+
+    let handleInputChange=(event)=>{
+        setformData((currData)=>{
+            return{...currData,[event.target.name]: event.target.value};
+        });
+    };
+
+    let handlesubmit=(event)=>{
+        console.log(formData);
+        event.preventDefault();
+        setformData({
+            username:"",
+            remarks:"",
+            rating:"",
+        })
+    }
     return(
         <div>
             <h4>give a comment</h4>
-            <form>
-                <input placeholder="username" type="text"/>
+            <form onSubmit={handlesubmit}>
+                <label htmlFor="username">username</label>
+                <input placeholder="username" 
+                 type="text"
+                  value={formData.username}
+                  onChange={handleInputChange} id="username"
+                  name="username"/>
                 <br></br><br></br>
-                <textarea>Remarks</textarea>
+
+                <label htmlFor="remarks">remarks</label>
+                <textarea placeholder="add a remarks" 
+                value={formData.remarks}
+                onChange={handleInputChange} id="remarks"
+                name="remarks"></textarea>
                 <br></br><br></br>
-                <input placeholder="rating" type="number" min={1} max={5}/>
+
+                <label htmlFor="rating">rating</label>
+                <input placeholder="rating"
+                 type="number" min={1} max={5} 
+                 value={formData.rating}
+                 onChange={handleInputChange} id="rating" name="rating"/>
                 <br></br><br></br>
+
                 <button>Add a comment!</button>
             </form>
         </div>
