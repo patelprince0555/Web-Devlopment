415925dbc5e234c02c9f412cf609058d7019d067
patelprince0555 <patelprince0555@gmail.com>
2024-10-24 18:57:12 +0530

    24-10-2024

--- 61-React-Day-61/react-state-class2/src/Form.jsx
+++ 61-React-Day-61/react-state-class2/src/Form.jsx
@@ -1,18 +1,56 @@
 import { useState } from "react";
 export default function Form(){
-    let[fullName ,setFullName]=useState("prince");
+    let[formData,setFormData]=useState({
+        fullName:"",
+        Username:""
+    })
 
-    let handleNameChange=(event)=>{
-        setFullName(event.target.value);
-    }
+    // let handleNameChange=(event)=>{
+    //     setFullName(event.target.value);
+    // }
+
+    // let handleUserName=(event)=>{
+    //     setUserName(event.target.value);
+    // }
+
+    let handleInputChange=(event)=>{
+        let fieldName=event.target.name;
+        let newValue=event.target.value;
+
+        setFormData((currData)=>{
+            currData[fieldName]=newValue;
+            return{...currData};
+        })
+    };
+
+    let handleSubmit=(event)=>{
+        event.preventDefault();
+        setFormData({
+            fullName:"",
+            Username:"",
+        });
+    };
     return(
-        <div>
-            <label htmlFor="username">FullName</label>
+        <div onSubmit={handleSubmit}>
+            <label htmlFor="fullName">FullName</label>
             <input placeholder="Enter your name" 
             type="text"
-             value={fullName}
-              onChange={handleNameChange}
-              id="username"/>
+             value={formData.fullName}
+              id="fullName"
+              name="fullName"
+              onChange={handleInputChange}/>
+              <br></br>
+              <br></br>
+
+            <label htmlFor="UserName">userName</label>
+            <input placeholder="Enter your name" 
+            type="text"
+             value={formData.Username}
+              id="UserName"
+              name="Username"
+              onChange={handleInputChange}/>
+              <br></br>
+              <br></br>
             <button>Submit</button>
         </div>
     )
