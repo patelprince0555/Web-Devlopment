b549e8badc17397539f98bb983067a75ecb2945d
patelprince0555 <patelprince0555@gmail.com>
2024-10-25 13:26:55 +0530

    25-10-2024

--- 61-React-Day-61/react-state-class2/src/App.jsx
+++ 61-React-Day-61/react-state-class2/src/App.jsx
@@ -1,12 +1,13 @@
 import Form from './Form'
 import './App.css'
 import CommentForm from './CommentsForm'
+import Counter from './Counter'
 
 function App() {
 
   return (
     <>
-      <CommentForm/>
+      <Counter/>
     </>
   )
 }
