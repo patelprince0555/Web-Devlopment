88654761e8311692f751410f04ec1ec2088d8a6a
patelprince0555 <patelprince0555@gmail.com>
2024-10-25 13:42:30 +0530

    25-10-2024

--- 61-React-Day-61/react-state-class2/src/Counter.jsx
+++ 61-React-Day-61/react-state-class2/src/Counter.jsx
@@ -1,19 +1,26 @@
 import { useState,useEffect } from "react";
 export default function Counter(){
-    let[count,setCount]=useState(0);
+    let[countx,setCountx]=useState(0);
+    let[county,setCounty]=useState(0);
 
-    let incCount=()=>{
-        setCount((currCount)=>currCount+1);
+    let incCountx=()=>{
+        setCountx((currCount)=>currCount+1);
+    };
+    let incCounty=()=>{
+        setCounty((currCount)=>currCount+1);
     };
 
     useEffect(function sideEffect(){
         console.log("this is the side effect");
-    });
+    },[countx]);
 
     return(
         <div>
-            <h3>count={count}</h3>
-            <button onClick={incCount}>+1</button>
+            <h3>count={countx}</h3>
+            <button onClick={incCountx}>+1</button>
+            <br></br>
+            <h3>count={county}</h3>
+            <button onClick={incCounty}>+1</button>
         </div>
     )
 } 
