e0a969b9ae8b6b0b62d3e36db010a8784ae6b9ea
patelprince0555 <patelprince0555@gmail.com>
2024-10-26 16:33:45 +0530

    26-10-2024

--- 62-React-Day-62/mini-project-react/src/SearchBox.jsx
+++ 62-React-Day-62/mini-project-react/src/SearchBox.jsx
@@ -5,6 +5,25 @@ import "./SearchBox.css";
 import { useState } from 'react';
 export default function SearchBox(){
     let[city,setCity]=useState("");
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY="a876ede7953902c7552fe767fde66ca7";
+
+    let getWeatherInfo=async()=>{
+        let response=await fetch(
+            `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        let jsonResponse=await response.json();
+        // console.log(jsonResponse);
+        let result={
+            temp:jsonResponse.main.temp,
+            tempMin:jsonResponse.main.temp_min,
+            tempMax:jsonResponse.main.temp_max,
+            Humidity:jsonResponse.main.humidity,
+            feelslike:jsonResponse.main.feels_like,
+            Weather:jsonResponse.weather[0].description,
+        };
+        console.log(result);
+    }
 
     let handleChange=(evt)=>{
         setCity(evt.target.value);
@@ -14,6 +33,7 @@ export default function SearchBox(){
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
     }
     return(
         <div className='searchbox'>
