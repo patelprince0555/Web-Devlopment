ca503111ac5efa6224c9207fd507da48532af599
patelprince0555 <patelprince0555@gmail.com>
2024-10-26 13:15:56 +0530

    26-10-2024

---
+++ 62-React-Day-62/mini-project-react/src/Button.jsx
@@ -0,0 +1,8 @@
+import Button from '@mui/material/Button';
+export default function Bottom(){
+    return(
+        <div>
+             <Button variant="contained">Contained</Button>
+        </div>
+    )
+}
